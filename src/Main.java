f5e9d8e02cfe773a7b4d7e83bd38ef1d305eb468
Coralie Zens <coralie.zens@ens.uvsq.fr>
2021-10-01 14:34:33 +0200

    Modification Main : message d'erreur de l'assertion

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ public class Main {
     public static void main(String[] argv) {
         System.out.println("Fraction :");
         Fraction f1 = new Fraction(3,4);
-        assert f1.toString().equals("{3/4}");
+        assert f1.toString().equals("{3/4}") : "Erreur dans Fraction.toString";
         System.out.println(f1.toString());
-
     }
 }
