611607a79606bddc8fbd9f0015cc261337883819
Coralie Zens <coralie.zens@ens.uvsq.fr>
2021-10-01 13:48:46 +0200

    Modification Getter-Setter Fraction et Assertion Main

--- src/Fraction.java
+++ src/Fraction.java
@@ -3,7 +3,24 @@ public class Fraction {
     private int numerateur ;
 
     public Fraction(int d, int n) {
+        /* cas des divisions par 0 --> NullDivisionException */
         this.denominateur = d;
+        this.numerateur = n;
+    }
+
+    public int getDen() {
+        return denominateur;
+    }
+
+    public int getNum() {
+        return numerateur;
+    }
+
+    public void setDen(int d) {
+        this.denominateur = d;
+    }
+
+    public void setNum(int n) {
         this.numerateur = n;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] argv) {
         System.out.println("Fraction :");
         Fraction f1 = new Fraction(3,4);
+        assert  f1.toString() == "{5/4}";
         System.out.println(f1.toString());
+
     }
 }
